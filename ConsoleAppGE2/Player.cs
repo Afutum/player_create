8e089cb040bc37a3808e5bb9d2f230519222535c
S-JOBI23-052\student <231116@st.yoshida-g.ac.jp>
2024-06-14 11:54:36 +0900

    プレイヤークラスの作成 #2

---
+++ ConsoleAppGE2/Player.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppGE2
+{
+    internal class Player
+    {
+        public string name { get; set; }
+        public int atk { get; set; }
+        public int def {  get; set; }
+        public int spd { get; set; }
+
+        public void CreatePlayer(string name,int atk,int def,int spd)
+        {
+            this.name = name;
+            this.atk = atk;
+            this.def = def;
+            this.spd = spd;
+        } 
+
+        public void ShowPlayer()
+        {
+            Console.WriteLine("プレイヤー名：" + name + "攻撃力：" + atk + "防御力：" + def + "すばやさ" + spd);
+            Console.ReadLine();
+        }
+    }
+}
